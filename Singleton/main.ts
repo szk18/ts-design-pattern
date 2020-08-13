368d146c2d37a93bc8d32401ecefa65196a58dce
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2020-08-13 10:37:25 +0900

    fix: == -> ===

--- Singleton/main.ts
+++ Singleton/main.ts
@@ -4,7 +4,7 @@ console.log("start");
 const obj1 = Singleton.getInstance();
 const obj2 = Singleton.getInstance();
 
-if (obj1 == obj2) console.log("obj1 == obj2");
+if (obj1 === obj2) console.log("obj1 == obj2");
 else console.log("obj1 != obj2");
 
 console.log("end");
